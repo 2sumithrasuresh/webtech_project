ea01b8e5bdba9b0cef0b99c2cfa50645b5b9450f
SharmishthaB <165554856+SharmishthaB@users.noreply.github.com>
2024-10-26 17:07:56 +0530

    Add files via upload

---
+++ myapp/src/components/AuthSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AuthSection = () => {
+  return (
+    <div className="auth-section">
+      
+      <div className="auth-buttons">
+        <button className="sign-up">Sign Up</button>
+        <button className="login">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default AuthSection;
