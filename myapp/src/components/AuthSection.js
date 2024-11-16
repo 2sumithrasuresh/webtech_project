495e5f676e77b58377f0227ca8a311a2c492fa5e
Sumithra <2sumithra.suresh@git.com>
2024-11-16 17:01:07 +0530

    Merged with new src

--- myapp/src/components/AuthSection.js
+++ myapp/src/components/AuthSection.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AuthSection = () => {
   return (
     <div className="auth-section">
-      
       <div className="auth-buttons">
-        <button className="sign-up">Sign Up</button>
-        <button className="login">Login</button>
+        <Link to="/signup">
+          <button className="sign-up">Sign Up</button>
+        </Link>
+        <Link to="/login">
+          <button className="login">Login</button>
+        </Link>
       </div>
     </div>
   );
